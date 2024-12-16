f9ea1c734be989aae1000862c72e1500b97c32f8
Asuka <88415985+Angel-Asuka@users.noreply.github.com>
2024-12-16 20:31:20 +0800

    Append json and yaml tags for configuration

--- config.go
+++ config.go
@@ -1,10 +1,10 @@
 package websvc
 
 type Config struct {
-	Host    string `mapstructure:"host"`     // [Optional] Listen host, default is all interfaces
-	Port    int    `mapstructure:"port"`     // [Optional] Listen port, default is 80 if SSL is not enabled, 443 otherwise
-	SSLCert string `mapstructure:"ssl_cert"` // [Optional] SSL Certificate file path
-	SSLKey  string `mapstructure:"ssl_key"`  // [Optional] SSL Key file path
+	Host    string `mapstructure:"host" json:"host" yaml:"host"`             // [Optional] Listen host, default is all interfaces
+	Port    int    `mapstructure:"port" json:"port" yaml:"port"`             // [Optional] Listen port, default is 80 if SSL is not enabled, 443 otherwise
+	SSLCert string `mapstructure:"ssl_cert" json:"ssl_cert" yaml:"ssl_cert"` // [Optional] SSL Certificate file path
+	SSLKey  string `mapstructure:"ssl_key" json:"ssl_key" yaml:"ssl_key"`    // [Optional] SSL Key file path
 }
 
 func (c *Config) IsSSL() bool {
