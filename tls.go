96b8a0a7bba864b17e5b138d5841f512a7520a66
Michael <michael@angelwork.org>
2026-03-17 11:06:26 +0800

    isolate cert verifier from NewTLSCertChecker to TLSCertVerifier,

--- tls.go
+++ tls.go
@@ -6,34 +6,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type TLSCertVerificationConfig struct {
-	TrustedCAs []string // The trusted CA certificates for TLS connections
-
-	// [Optional] Custom certificate checker
-	// if specified, will be used to verify the remote TLS certificate, return nil if the certificate is valid, otherwise return an error
-	// The error message will be pass to the client with HTTP status code 403
-	// if not specified, the checker will only check if the certificate is signed by one of the trusted CAs, and has the correct key usage for client authentication
-	CertChecker func(*gin.Context, *x509.Certificate) error
+type TLSCertVerifier struct {
+	opts x509.VerifyOptions
 }
 
-func NewTLSCertVerifier(cfg *TLSCertVerificationConfig) gin.HandlerFunc {
+func NewTLSCertVerifier(trustedCAs []string) *TLSCertVerifier {
 	cas := x509.NewCertPool()
-	for _, ca := range cfg.TrustedCAs {
-		cas.AppendCertsFromPEM([]byte(ca))
-	}
-
-	opts := x509.VerifyOptions{
-		Roots:     cas,
-		KeyUsages: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
-	}
-
-	certChecker := cfg.CertChecker
-	if certChecker == nil {
-		certChecker = func(c *gin.Context, cert *x509.Certificate) error {
-			return nil
+	for _, ca := range trustedCAs {
+		cacert, e := loadData(ca)
+		if e == nil && cacert != nil {
+			cas.AppendCertsFromPEM(cacert)
 		}
 	}
+	return &TLSCertVerifier{
+		opts: x509.VerifyOptions{
+			Roots:     cas,
+			KeyUsages: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		},
+	}
+}
 
+func (v *TLSCertVerifier) Verify(cert *x509.Certificate) error {
+	_, e := cert.Verify(v.opts)
+	return e
+}
+
+func NewTLSCertChecker(checker func(*gin.Context, *x509.Certificate) error) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if c.Request.TLS == nil || len(c.Request.TLS.PeerCertificates) == 0 {
 			c.String(400, "TLS client certificate required")
@@ -42,14 +40,7 @@ func NewTLSCertVerifier(cfg *TLSCertVerificationConfig) gin.HandlerFunc {
 		}
 
 		clientCert := c.Request.TLS.PeerCertificates[0]
-		_, err := clientCert.Verify(opts)
-		if err != nil {
-			c.String(403, "Invalid TLS client certificate")
-			c.Abort()
-			return
-		}
-
-		err = certChecker(c, clientCert)
+		err := checker(c, clientCert)
 		if err != nil {
 			c.String(403, err.Error())
 			c.Abort()
